79f1c3f72eda0c443c529f80b712f8c9a849c43c
Harsh Dubey <131514056+harshdubey6@users.noreply.github.com>
2024-12-24 10:41:06 +0530

    Update Dashboard.jsx

--- frontend/pages/Dashboard.jsx
+++ frontend/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
   useEffect(() => {
     const fetchBalance = async () => {
       const res = await axios.get(
-        "http://localhost:3000/api/v1/account/balance",
+        "https://paytm-backend-8u9f.vercel.app/api/v1/account/balance",
         {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
