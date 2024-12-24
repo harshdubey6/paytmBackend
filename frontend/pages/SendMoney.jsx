c100a40648ba340d1bbc967d018be47428a2c3a7
Harsh Dubey <131514056+harshdubey6@users.noreply.github.com>
2024-12-24 15:54:08 +0530

    Update SendMoney.jsx

--- frontend/pages/SendMoney.jsx
+++ frontend/pages/SendMoney.jsx
@@ -45,7 +45,7 @@ export default function SendMoney() {
               <button
                 onClick={() => {
                   axios.post(
-                    "https://paytmbackend-84sc.onrender.com/api/v1/account/transfer",
+                    "https://paytmbackend-84sc.onrender.com/account/transfer",
                     {
                       to: id,
                       amount,
