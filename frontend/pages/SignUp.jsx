59a2a465dc5369cfc617af6da642533da6a247dc
Harsh Dubey <131514056+harshdubey6@users.noreply.github.com>
2024-12-24 15:35:12 +0530

    Update SignUp.jsx

--- frontend/pages/SignUp.jsx
+++ frontend/pages/SignUp.jsx
@@ -53,7 +53,7 @@ export default function SignUp() {
             <Button
               onClick={async () => {
                 const res = await axios.post(
-                  "https://paytm-backend-8u9f.vercel.app/api/v1/user/signup",
+                  "https://paytmbackend-84sc.onrender.com/api/v1/user/signup",
                   {
                     username: username,
                     password: password,
